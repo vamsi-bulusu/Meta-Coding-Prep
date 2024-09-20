567f5837e7586c5df4301dcaf89dba38382a55e0
Vamsi Krishna <bulusu.vk08@gmail.com>
2024-09-19 23:31:44 -0700

    Solved Greedy Module

---
+++ greedy/slowSums.py
@@ -0,0 +1,48 @@
+
+def getTotalTime(arr):
+  arr.sort(reverse = True)
+  totalPenalty = 0
+  for i in range(1, len(arr)):
+    arr[i] = arr[i] + arr[i - 1]
+    totalPenalty += arr[i]
+  return totalPenalty
+   
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+
+def printInteger(n):
+  print('[', n, ']', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printInteger(expected)
+    print(' Your output: ', end='')
+    printInteger(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  arr_1 = [4, 2, 1, 3]
+  expected_1 = 26
+  output_1 = getTotalTime(arr_1)
+  check(expected_1, output_1)
+
+  arr_2 = [2, 3, 9, 8, 4]
+  expected_2 = 88
+  output_2 = getTotalTime(arr_2)
+  check(expected_2, output_2)
+
+  # Add your own test cases here
+  
